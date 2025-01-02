7b7bef67000457425e5e03875f3ab46fc8de2a57
mdelgert <mdelgert@users.noreply.github.com>
2025-01-02 21:30:45 +0000

    deploy: 74eebf2e248f3d3bfb60d72691f595b06c9e6ded

--- assets/about-C4X2QYZs.js
+++ assets/about-C4X2QYZs.js
@@ -2,8 +2,6 @@ import"./style-z09VfpJW.js";document.querySelector("#app").innerHTML=`
   <div>
     <h1>About</h1>
     <p>Hello World!</p>
-    <a href="./index.html">Home1</a>
-    <a href="index.html">Home2</a>
-    <a href="/vite-deploy-demo/index.html">Home3</a>
+    <a href="index.html">Home</a>
   </div>
 `;
